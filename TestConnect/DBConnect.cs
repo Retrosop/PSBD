af870f2df2373f2d84e512bb2d3791f630344167
Glagolev Vladimir <37924155+Retrosop@users.noreply.github.com>
2023-09-15 09:28:26 +1000

    Connecttosqllite

---
+++ TestConnect/DBConnect.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Data.Sqlite;
+
+namespace HelloApp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			using (var connection = new SqliteConnection("Data Source=usersdata.db"))
+			{
+				connection.Open();
+			}
+			Console.Read();
+		}
+	}
+}
