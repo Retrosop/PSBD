e0660030a363089d94ca9b856a2832772d526d61
Alexander Malik <malik101112@mail.ru>
2023-12-18 22:27:32 +1000

    upld

---
+++ malik.a/wpf/FurnitureApp/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Data;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+
+namespace FurnitureApp
+{
+	public partial class MainWindow : Window
+	{
+
+		public class YourDataObject
+		{
+			public string Column1 { get; set; }
+			public string Column2 { get; set; }
+		}
+
+		private readonly MySqlHelper mySqlHelper;
+
+		public MainWindow()
+		{
+			InitializeComponent();
+
+			string connectionString = "Server=localhost;Database=my_furniture_factory;User ID=root;Password='';";
+			mySqlHelper = new MySqlHelper(connectionString);
+
+			RefreshData();
+
+		}
+
+		private void RefreshData()
+		{
+			string selectCategoryQuery = "SELECT * FROM category";
+			DataTable categoryTable = mySqlHelper.ExecuteQuery(selectCategoryQuery);
+			categoryDataGrid.ItemsSource = categoryTable.DefaultView;
+
+			string selectFurnitureQuery = "SELECT * FROM furniture";
+			DataTable furnitureTable = mySqlHelper.ExecuteQuery(selectFurnitureQuery);
+			furnitureDataGrid.ItemsSource = furnitureTable.DefaultView;
+		}
+
+		private void AddCategoryButton_Click(object sender, RoutedEventArgs e)
+		{
+			string insertCategoryQuery = "INSERT INTO category (name) VALUES ('New Category')";
+			int rowsAffected = mySqlHelper.ExecuteNonQuery(insertCategoryQuery);
+			MessageBox.Show($"{rowsAffected} rows added");
+			RefreshData();
+		}
+
+		private void RefreshCategoriesButton_Click(object sender, RoutedEventArgs e)
+		{
+			RefreshData();
+		}
+
+		private void DeleteCategoryButton_Click(object sender, RoutedEventArgs e)
+		{
+			DataRowView selectedRow = (DataRowView)categoryDataGrid.SelectedItem;
+
+			if (selectedRow != null)
+			{
+				int categoryId = Convert.ToInt32(selectedRow["id"]);
+				string deleteCategoryQuery = $"DELETE FROM category WHERE id = {categoryId}";
+				int rowsAffected = mySqlHelper.ExecuteNonQuery(deleteCategoryQuery);
+				MessageBox.Show($"{rowsAffected} rows deleted");
+				RefreshData();
+			}
+			else
+			{
+				MessageBox.Show("Выберите категорию для удаления.");
+			}
+		}
+
+		private void AddFurnitureButton_Click(object sender, RoutedEventArgs e)
+		{
+			string insertFurnitureQuery = "INSERT INTO furniture (name, description, category_id, price) VALUES ('New Furniture', 'Description', 1, 0.00)";
+			int rowsAffected = mySqlHelper.ExecuteNonQuery(insertFurnitureQuery);
+			MessageBox.Show($"{rowsAffected} rows added");
+			RefreshData();
+		}
+
+		private void RefreshFurnitureButton_Click(object sender, RoutedEventArgs e)
+		{
+			RefreshData();
+		}
+
+		private void DeleteFurnitureButton_Click(object sender, RoutedEventArgs e)
+		{
+			DataRowView selectedRow = (DataRowView)furnitureDataGrid.SelectedItem;
+
+			if (selectedRow != null)
+			{
+				int furnitureId = Convert.ToInt32(selectedRow["id"]);
+				string deleteFurnitureQuery = $"DELETE FROM furniture WHERE id = {furnitureId}";
+				int rowsAffected = mySqlHelper.ExecuteNonQuery(deleteFurnitureQuery);
+				MessageBox.Show($"{rowsAffected} rows deleted");
+				RefreshData();
+			}
+			else
+			{
+				MessageBox.Show("Выберите запись о мебели для удаления.");
+			}
+		}
+	}
+}
---
+++ malik.a/wpf/FurnitureApp/MySqlHelper.cs
@@ -0,0 +1,67 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Data;
+
+namespace FurnitureApp
+{
+	public class MySqlHelper
+	{
+		private readonly MySqlConnection _connection;
+
+		public MySqlHelper(string connectionString)
+		{
+			_connection = new MySqlConnection(connectionString);
+		}
+
+		public DataTable ExecuteQuery(string query)
+		{
+			DataTable dataTable = new DataTable();
+
+			try
+			{
+				_connection.Open();
+				using (MySqlCommand command = new MySqlCommand(query, _connection))
+				{
+					using (MySqlDataAdapter adapter = new MySqlDataAdapter(command))
+					{
+						adapter.Fill(dataTable);
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Error: {ex.Message}");
+			}
+			finally
+			{
+				_connection.Close();
+			}
+
+			return dataTable;
+		}
+
+		public int ExecuteNonQuery(string query)
+		{
+			int rowsAffected = 0;
+
+			try
+			{
+				_connection.Open();
+				using (MySqlCommand command = new MySqlCommand(query, _connection))
+				{
+					rowsAffected = command.ExecuteNonQuery();
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine($"Error: {ex.Message}");
+			}
+			finally
+			{
+				_connection.Close();
+			}
+
+			return rowsAffected;
+		}
+	}
+}
